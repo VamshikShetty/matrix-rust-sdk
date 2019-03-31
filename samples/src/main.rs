ab512d72ae276580611ded929ed195ca76c9751d
Vamshik Shetty <vamshikdshetty@gmail.com>
2019-04-01 01:22:48 +0530

    simple login flow sample added

--- samples/src/main.rs
+++ samples/src/main.rs
@@ -1,5 +1,30 @@
 extern crate matrix;
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+
+use futures::Future;
+use hyper::client::HttpConnector;
+use hyper::Client;
+use matrix::apis::Error;
+use tokio_core::reactor::Core;
 
 fn main() {
-    println!("samples are here");
+    let mut core = Core::new().expect("failed to init core");
+    let handle = core.handle();
+
+    let apicli = matrix::apis::client::APIClient::new(
+        matrix::apis::configuration::Configuration::new(
+            Client::configure()
+                .connector(HttpConnector::new(4, &handle))
+                .build(&handle),
+        ),
+    );
+
+    let work = apicli.session_management_api().get_login_flows().and_then(|resp|{
+            println!("{:?}", resp.flows);
+            futures::future::ok(())
+        });
+    
+    core.run(work).expect("failed to run core");
 }
