6c01ec00b30267d67775af19c3f0f5cf8ad223c8
Vamshik Shetty <vamshikdshetty@gmail.com>
2019-04-01 21:49:18 +0530

    corrections

--- matrix/examples/login_get.rs
+++ matrix/examples/login_get.rs
@@ -1,25 +1,12 @@
 
 extern crate matrix;
-// extern crate futures;
-// extern crate hyper;
-// extern crate tokio_core;
 
-// use futures::Future;
-// use hyper::client::HttpConnector;
-// use hyper::Client;
-// use tokio_core::reactor::Core;
-
-use matrix::models::Model200LoginGet;
+use matrix::apis::client::APIClient;
+use matrix::apis::configuration::Configuration;
 
 fn main() {
-    // let mut core = Core::new().expect("failed to init core");
-    // let handle = core.handle();
 
-    let apicli = matrix::apis::client::APIClient::new(
-        matrix::apis::configuration::Configuration::new(
-        )
-    );
-
+    let apicli = APIClient::new( Configuration::new());
     let res = apicli.session_management_api().get_login_flows(); 
     println!("{:?}", res);
 }
--- matrix/examples/login_put.rs
+++ matrix/examples/login_put.rs
@@ -1,25 +1,16 @@
 
 extern crate matrix;
-// extern crate futures;
-// extern crate hyper;
-// extern crate tokio_core;
-
-// use hyper::client::HttpConnector;
-// use hyper::Client;
-// use tokio_core::reactor::Core;
 
 use matrix::models::LoginRequestBody;
 use matrix::models::UserIdentifier;
 
 fn main() {
-    // let mut core = Core::new().expect("failed to init core");
-    // let handle = core.handle();
 
     let apicli = matrix::apis::client::APIClient::new(
         matrix::apis::configuration::Configuration::new()
     );
 
-    let mut resq_body = LoginRequestBody {
+    let resq_body = LoginRequestBody {
         _type: "m.login.password".to_string(),
         identifier: Some(UserIdentifier { 
             _type: "m.id.user".to_string(), 
@@ -35,6 +26,5 @@ fn main() {
     };
 
     let res = apicli.session_management_api().login(resq_body); 
-
     println!("{:?}", res);
 }
